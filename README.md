f13279895139cac05e8cd8b23b06593453f6ecf4
Tobias Eriksson <tobias.eriksson@studerande.karlskogafhs.se>
2023-05-17 12:01:11 +0200

    Latest update

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
-# Sass-base-2022
+# Streamify
 
-## Clone to update base system
-`https://github.com/Tobiaseriksson98/Sass-base-2022.git .`
-
-## Use as new project
-
-`git clone https://github.com/Tobiaseriksson98/Sass-base-2022.git . && rm -rf .git`
 
 ## Develop
-`sass --watch src/scss/style.scss style.css`# streamify
+`sass --watch src/scss/style.scss style.css`
---
+++ js/otherOptions.js
@@ -0,0 +1,18 @@
+const otherOptions = () => {
+
+    const otherOptionsButton = document.querySelector('.other-options__all')
+    const theOption1 = document.querySelector('.other-options__facebook')
+    const theOption2 = document.querySelector('.other-options__twitter')
+    const theOption3 = document.querySelector('.other-options__gmail')
+
+    const togglePopup = () => {
+        theOption1.classList.toggle('other-options__facebook--visible')
+        theOption2.classList.toggle('other-options__twitter--visible')
+        theOption3.classList.toggle('other-options__gmail--visible')
+    }
+
+    otherOptionsButton.addEventListener('mouseover', togglePopup)
+
+}
+
+otherOptions()
